e90d4c39c4325959f17d666cfc0befa6be009420
Stephane Orluc <stephane.orluc@forgerock.com>
2019-07-05 11:18:32 +0200

    Copyright date update

--- src/main/java/com/sorluc/InWeboAuthNode/InWeboPushNodePlugin.java
+++ src/main/java/com/sorluc/InWeboAuthNode/InWeboPushNodePlugin.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2017-2018 ForgeRock AS.
+ * Copyright 2017-2019 ForgeRock AS.
  */
 
 package com.sorluc.InWeboAuthNode;
